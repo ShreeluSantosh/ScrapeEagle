0a8e92686d1aae5ca453940c763be6e59b0320b6
Shreelu Santosh <94289402+ShreeluSantosh@users.noreply.github.com>
2023-12-25 21:58:41 +0530

    Update App.js
    Added option for scraping img URLs and download button for downloading scraped URLs in txt file format

--- frontend-app/src/App.js
+++ frontend-app/src/App.js
@@ -9,14 +9,14 @@ function App() {
     setUrl(event.target.value);
   };
 
-  const handleScrape = async () => {
+  const handleScrape = async (option) => {
     try {
       const response = await fetch('http://127.0.0.1:5000/scrape', {
         method: 'POST',
         headers: {
           'Content-Type': 'application/json',
         },
-        body: JSON.stringify({ url }),
+        body: JSON.stringify({ url, option }),
       });
 
       const result = await response.json();
@@ -31,6 +31,19 @@ function App() {
     }
   };
 
+  const handleDownload = () => {
+    // Create a Blob with the content and trigger a download
+    const blob = new Blob([output], { type: 'text/plain' });
+    const url = window.URL.createObjectURL(blob);
+    const a = document.createElement('a');
+    a.href = url;
+    a.download = 'scraped_urls.txt';
+    document.body.appendChild(a);
+    a.click();
+    document.body.removeChild(a);
+    window.URL.revokeObjectURL(url);
+  };
+
   return (
     <div className="App">
       <h1 className="App-title">ScrapeVulture</h1>
@@ -38,13 +51,20 @@ function App() {
         <div className="input-box">
           <label>Enter URL:</label>
           <input type="text" value={url} onChange={handleUrlChange} />
-          <button onClick={handleScrape}>Scrape</button>
+          <button onClick={() => handleScrape('links')}>Scrape Links</button>
+          <button onClick={() => handleScrape('images')}>Scrape Images</button>
+          <button onClick={() => handleScrape('metadata')}>Scrape Metadata</button>
         </div>
         <div className="output-box">
-          <h2 className="label-output">List of URLs Scraped:</h2>
+          <h2 className="label-output">Scraped Content:</h2>
           <div className="url-box">
             <pre className="scrollable-box">{output}</pre>
           </div>
+          {output && (
+            <div className="download-box">
+              <button onClick={handleDownload}>Download URLs</button>
+            </div>
+          )}
         </div>
       </div>
     </div>
